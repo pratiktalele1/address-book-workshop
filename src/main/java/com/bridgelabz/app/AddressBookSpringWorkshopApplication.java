0f8453ff237c8b04e47c692db4c1de9d7d0ae8d6
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-22 08:18:14 +0530

    welcome to addressbook workshop

---
+++ src/main/java/com/bridgelabz/app/AddressBookSpringWorkshopApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSpringWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSpringWorkshopApplication.class, args);
+		System.out.println("hello from workshop spring boot app.");
+
+	}
+
+}
