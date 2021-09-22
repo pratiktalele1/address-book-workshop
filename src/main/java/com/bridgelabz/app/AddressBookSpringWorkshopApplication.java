c3d50cdc28df04331564fd1f3ac5336a4212d9ee
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-22 08:21:55 +0530

    lombok is added to project

--- src/main/java/com/bridgelabz/app/AddressBookSpringWorkshopApplication.java
+++ src/main/java/com/bridgelabz/app/AddressBookSpringWorkshopApplication.java
@@ -3,13 +3,15 @@ package com.bridgelabz.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication @Slf4j
 public class AddressBookSpringWorkshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookSpringWorkshopApplication.class, args);
 		System.out.println("hello from workshop spring boot app.");
-
+		log.info("lombok is added to project..!");
 	}
 
 }
